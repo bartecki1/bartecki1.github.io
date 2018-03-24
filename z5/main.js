d336c44bb3e629616e85bbd85760539b609b9118
bartecki1 <37475985+bartecki1@users.noreply.github.com>
2018-03-24 15:02:54 +0100

    Add files via upload

---
+++ z5/main.js
@@ -0,0 +1,25 @@
+//var imie="moje imie";
+//alert("Siema "+imie);
+
+//var wiek=prompt("ile masz lat");
+
+
+
+}
+//var click=function(){
+//	document.querySelector('aside').style.color = 'blue';
+
+}
+var nav1 = function(){
+	document.querySelector('main').style.color = 'red';
+}
+
+var nav2 = function(){
+	document.querySelector('main').style.color = 'pink';
+}
+var nav3 = function(){
+	document.querySelector('main').style.color = 'blue';
+}
+var nav4 = function(){
+	document.querySelector('main').style.color = 'lime';
+}
