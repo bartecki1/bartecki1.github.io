9f811949696d74c2c662c8e40c7d5d20d4af4e20
bartecki1 <37475985+bartecki1@users.noreply.github.com>
2018-04-14 13:48:52 +0200

    Add files via upload

--- z5/main.js
+++ z5/main.js
@@ -5,17 +5,20 @@
 
 
 
-}
+//}
 //var click=function(){
 //	document.querySelector('aside').style.color = 'blue';
+var wys = 100;
+var dlu = 200;
+var x = 13;
+var y = 613;
 
-}
 var nav1 = function(){
 	document.querySelector('main').style.color = 'red';
 }
 
 var nav2 = function(){
-	document.querySelector('main').style.color = 'pink';
+	document.querySelector('main').style.color = 'orange';
 }
 var nav3 = function(){
 	document.querySelector('main').style.color = 'blue';
@@ -23,3 +26,40 @@ var nav3 = function(){
 var nav4 = function(){
 	document.querySelector('main').style.color = 'lime';
 }
+var nav5 = function(){
+	document.querySelector('main').style.color = 'white';
+}
+var nav6 = function(){
+	document.querySelector('main').style.color = 'black';
+}
+
+
+
+function plus(w, d) {
+	wys += w;
+	dlu += d;
+	var tw = wys +'px';
+	var td = dlu +'px';
+	document.querySelector('img').style.height = tw;
+	document.querySelector('img').style.width = td;
+}
+
+function lol(w, d) {
+	wys += w;
+	dlu += d;
+	var tw = wys +'px';
+	var td = dlu +'px';
+	document.querySelector('img').style.height = tw;
+	document.querySelector('img').style.width = td;
+
+}
+
+
+	function szukaj (){
+		var zapytanie = document.getElementById('zapytanie').value;
+		var link = 'http://maps.google.com/?q=';
+		window.open(link+zapytanie)
+	
+}
+
+	
